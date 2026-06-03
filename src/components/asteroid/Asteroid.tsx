71e861686181101d74f68834feaaf905bf28f40c
Evgeny Loy <evgenyloy@gmail.com>
2026-06-04 01:10:20 +0300

    add img tag with image

--- src/components/asteroid/Asteroid.tsx
+++ src/components/asteroid/Asteroid.tsx
@@ -1,9 +1,10 @@
-import { useGetAsteroidQuery } from '../../api/apiSlice';
-import { useAppSelector } from '../../hooks/hooks';
-import AsteroidComposeComponent from './AsteroidComposeComponent';
-import Spinner from '../spinner/Spinner';
-import Error from '../error/Error';
-import './asteroid.scss';
+import { useGetAsteroidQuery } from "../../api/apiSlice";
+import { useAppSelector } from "../../hooks/hooks";
+import AsteroidComposeComponent from "./AsteroidComposeComponent";
+import Spinner from "../spinner/Spinner";
+import Error from "../error/Error";
+import asteroidBg from "../../../public/images/m2.jpg";
+import "./asteroid.scss";
 
 function Asteroid() {
   const asteroidId = useAppSelector((state) => state.asteroidsSlice.id);
@@ -17,6 +18,7 @@ function Asteroid() {
 
   return (
     <div className="asteroid">
+      <img src={asteroidBg} alt="asteroid" className="asteroid__image" />
       {(isFetching || isLoading) && <Spinner />}
       {isError && <Error />}
       {isSuccess && <AsteroidComposeComponent asteroid={asteroid} />}
